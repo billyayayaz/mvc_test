39b7994a3734c72fb1bb156510f8c27e26fe1c2d
billyayayaz <billyayayaz@gmail.com>
2021-01-23 16:37:59 +0800

    modify

---
+++ CoreMvc3_firstMVC/CoreMvc3_firstMVC/Models/product.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CoreMvc3_firstMVC.Models
+{
+    public class product
+    {
+        public int Id { get; set; }
+        public string ProductName { get; set; }
+        public int UnitPrice { get; set; }
+    }
+}
